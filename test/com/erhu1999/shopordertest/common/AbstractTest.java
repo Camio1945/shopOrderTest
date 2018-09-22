04f16adbe4758d6492ee2ed4838bb604c03af535
erhu1999 <837087787@qq.com>
2018-09-22 19:31:02 +0800

    v006-普通的Thread多线程改为Java8的parallelStream并发流

--- test/com/erhu1999/shopordertest/common/AbstractTest.java
+++ test/com/erhu1999/shopordertest/common/AbstractTest.java
@@ -1,5 +1,9 @@
 package com.erhu1999.shopordertest.common;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
 /**
  * 抽象出一个测试的父类
  */
@@ -29,4 +33,14 @@ public abstract class AbstractTest {
         p("\n------------------------------ 分隔线 " + classSingleName + " : " + methodName + " ------------------------------");
     }
 
+    /**
+     * 获取一个整型的并发的流
+     *
+     * @param size 列表大小
+     * @return
+     */
+    public static Stream<Integer> parallelIntegerStream(int size) {
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        return Arrays.asList(new Integer[size]).parallelStream().map(i -> atomicInteger.incrementAndGet());
+    }
 }
