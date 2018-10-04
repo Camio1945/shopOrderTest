479bb74379c9dee7f23ee285ce5d039211374eb4
erhu1999 <837087787@qq.com>
2018-10-04 17:52:15 +0800

    删除同步代码后重新测试

--- java/com/erhu1999/shopordertest/version016/GoodsCache.java
+++ java/com/erhu1999/shopordertest/version016/GoodsCache.java
@@ -18,20 +18,8 @@ class GoodsCache {
      * @param goodsId 商品ID
      * @return 商品对象
      */
-    public static synchronized Map<String, Object> getGoodsById(long goodsId) {
+    public static Map<String, Object> getGoodsById(long goodsId) {
         return goodsCacheMap.get(goodsId);
-    }
-
-    /**
-     * 减少库存
-     *
-     * @param goodsId  商品ID
-     * @param subStock 减少库存数量
-     */
-    public static synchronized void subStock(long goodsId, int subStock) {
-        Map<String, Object> goods = getGoodsById(goodsId);
-        int stock = (Integer) goods.get("stock");
-        goods.put("stock", stock - subStock);
     }
 
     /**
