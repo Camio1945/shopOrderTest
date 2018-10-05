9c59ccba6b258beba05b79f21c79946866eced9c
erhu1999 <837087787@qq.com>
2018-10-05 19:53:55 +0800

    017-数据库ID为long(20)类型与int(10)类型的对比

--- test/com/erhu1999/shopordertest/common/AbstractTest.java
+++ test/com/erhu1999/shopordertest/common/AbstractTest.java
@@ -1,6 +1,7 @@
 package com.erhu1999.shopordertest.common;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
@@ -8,6 +9,9 @@ import java.util.stream.Stream;
  * 抽象出一个测试的父类
  */
 public abstract class AbstractTest {
+    /** 测试次数 */
+    public static final int TEST_TIMES = 12;
+
     /**
      * 打印
      *
@@ -42,5 +46,30 @@ public abstract class AbstractTest {
     public static Stream<Integer> parallelIntegerStream(int size) {
         AtomicInteger atomicInteger = new AtomicInteger(0);
         return Arrays.asList(new Integer[size]).parallelStream().map(i -> atomicInteger.incrementAndGet());
+    }
+
+    /** 获取去掉一个最大值、去掉一个最小值后的平均数（即去尾平均数） */
+    public static double getTrimmedMean(List<Long> longList) {
+        long min = longList.stream().mapToLong(Long::longValue).min().getAsLong();
+        long max = longList.stream().mapToLong(Long::longValue).max().getAsLong();
+        long[] removedMinAndMax = {-1, -1};
+        double trimmedMean = longList.stream()
+                .filter(l -> {
+                    long longValue = l.longValue();
+                    if (removedMinAndMax[0] == -1 && longValue == min) {
+                        removedMinAndMax[0] = longValue;
+                        return false;
+                    }
+                    if (removedMinAndMax[1] == -1 && longValue == max) {
+                        removedMinAndMax[1] = longValue;
+                        return false;
+                    }
+                    return true;
+                })
+                .mapToLong(Long::longValue)
+                .average()
+                .getAsDouble();
+        System.out.println("去掉最小值：" + removedMinAndMax[0] + "  最大值：" + removedMinAndMax[1]);
+        return trimmedMean;
     }
 }
